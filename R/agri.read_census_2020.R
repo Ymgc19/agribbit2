84a2061b9ae6b77e16f04ee6ca15bac838607b55
Ymgc19 <username@example.com>
2024-12-06 20:54:53 +0900

    関数名の変更

--- R/agri.read_census_2020.R
+++ R/agri.read_census_2020.R
@@ -1,9 +1,9 @@
 # 農林業センサスのデータを読み込んでそのままオブジェクト化
 #' @title read census data as object
-#' @description \code{agri.read_census}
+#' @description \code{agri.read_census_2020}
 #' @export
 
-agri.read_census <- function(pref_code){
+agri.read_census_2020 <- function(pref_code){
   agribbit2::agri.collect_census(pref_code)
   if (pref_code <= 9){
     pref_code <- as.character(paste("0", pref_code, sep = ""))

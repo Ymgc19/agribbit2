fad8bf80f31ef2bd521d84d5ffbded9112d74120
Ymgc19 <username@example.com>
2025-09-01 14:02:14 +0900

    dirを指定できるように改築

--- R/agri.read_census_2020.R
+++ R/agri.read_census_2020.R
@@ -3,7 +3,7 @@
 #' @description \code{agri.read_census_2020}
 #' @export
 
-agri.read_census_2020 <- function(pref_code){
+agri.read_census_2020 <- function(pref_code, dir = ""){
   agribbit2::agri.collect_census(pref_code)
   if (pref_code <= 9){
     pref_code <- as.character(paste("0", pref_code, sep = ""))
@@ -11,13 +11,11 @@ agri.read_census_2020 <- function(pref_code){
   else{
     pref_code <- pref_code
   }
-  download_dir <- paste(as.character(pref_code), "農林業センサス2020", sep = "")
+  download_dir <- paste(dir, as.character(pref_code), "農林業センサス2020", sep = "")
   df <- agribbit2::agri.read_as_csv(download_dir)
   unlink(download_dir, recursive = T)
   df <- df %>%
     mutate(KEY_CODE = as.numeric(KEY_CODE))
   return(df)
 }
-
-
 
